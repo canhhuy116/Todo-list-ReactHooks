2e14bb66f8dfe333381ce7fb2b701cd6530c55d2
canhhuy116 <canhhuy1162002@gmail.com>
2022-07-16 14:29:49 +0700

    Create app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from 'react';
+import Input from './components/Input/Input';
+import TodoList from './components/TodoList/TodoList';
+import './App.scss';
+import { v4 } from 'uuid';
 
-function App() {
+const TO_DO_LIST_STORAGE = 'TodoList';
+
+interface Job {
+  id: string;
+  name: string;
+}
+
+function App(): JSX.Element {
+  let a: string | null;
+  a = null;
+
+  const [todoList, setTodoList] = useState<Job[]>([]);
+
+  useEffect(() => {
+    const ListStorage = localStorage.getItem(TO_DO_LIST_STORAGE);
+    if (ListStorage) {
+      console.log(JSON.parse(ListStorage));
+      setTodoList(JSON.parse(ListStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log('123');
+    localStorage.setItem(TO_DO_LIST_STORAGE, JSON.stringify(todoList));
+  }, [todoList]);
+
+  const onListChangeCB = useCallback(
+    (name: string) => {
+      setTodoList([...todoList, { id: v4(), name }]);
+    },
+    [todoList]
+  );
+
+  const onClickDelBtn = useCallback((job: Job) => {
+    setTodoList((prevState) => {
+      return prevState.filter((todo) => {
+        return job.id !== todo.id;
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{a}</h1>
+      <h2>To do List</h2>
+      <Input onListChange={onListChangeCB} />
+      <TodoList todoList={todoList} onClickDelBtn={onClickDelBtn} />
     </div>
   );
 }
